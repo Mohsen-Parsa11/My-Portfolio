f8e90d317e77f51a30659aa221217089c85fcdf7
Mohsen Parsa <mohsenp249@gmail.com>
2025-04-12 11:16:01 -0900

    react store project added

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -4,6 +4,7 @@ import store from "../assets/store.jpg";
 import shortly from "../assets/shortly.jpg";
 import card from "../assets/card.jpg";
 import realstate from "../assets/realState.jpg"
+import homePage from "../assets/homePage.jpg"
 
 function Project() {
   return (
@@ -86,6 +87,21 @@ function Project() {
                 <div className=" group-hover:bg-black-500 group-hover:bg-gradient-to-t group-hover:from-black group-hover:to-transparent transition duration-500 w-full group-hover:opacity-90 h-20 py-3 px-2">
                     <h4 className="text-lg  lg:text-3xl hidden group-hover:block text-white z-20 ">
                       Real State React Project
+                    </h4>
+                  </div>
+                </div>
+              </a>
+
+              <a href="https://product-store-project-85rr-l4o92z7k1-mohsen-parsa11s-projects.vercel.app/" className="border border-sky-400 rounded shadow shadow-sky-400 relative group">
+                <img
+                  src={homePage}
+                  alt=""
+                  className="w-full object-cover rounded lg:h-75"
+                />
+                <div className="flex justify-center items-end absolute top-0 bottom-0 left-0 right-0 z-50 ">
+                <div className=" group-hover:bg-black-500 group-hover:bg-gradient-to-t group-hover:from-black group-hover:to-transparent transition duration-500 w-full group-hover:opacity-90 h-20 py-3 px-2">
+                    <h4 className="text-lg  lg:text-3xl hidden group-hover:block text-white z-20 ">
+                      Product Store React Project
                     </h4>
                   </div>
                 </div>
